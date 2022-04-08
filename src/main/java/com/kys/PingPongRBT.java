5d62c831873780583fb31cfc3f26420694cd4c1b
danny <kys205481@gmail.com>
2022-04-08 20:05:27 +0800

    init

--- src/main/java/com/kys/PingPongRBT.java
+++ src/main/java/com/kys/PingPongRBT.java
@@ -1,20 +1,120 @@
 package com.kys;
 
+import com.kys.robot001.Robot001Application;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.permission.Role;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.entity.user.UserStatus;
+import org.springframework.boot.SpringApplication;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PingPongRBT {
 
+
+    private static final Long serverId = 123L;// me server
+    private static final Long ownerId = 123L;// me
+    //private static final Long serverId = ***;// yixian server
+    //private static final Long ownerId = ****;// yixian
+
     public static void main(String[] args) {
-        PingPongRBT.main(args);
+
+        SpringApplication.run(Robot001Application.class, args);
+
+        // Log the bot in
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken("OTU3ODc1MTI0OTM2MjQ1MjU4.YkFIug.A09sioSH-opW0c7Oj1KwSB1IBMg")
+                .setAllIntents()
+                .login().join();
+
+        int memberCount = api.getServerById(serverId).get().getMemberCount();
+        Server server = api.getServerById(serverId).get();
+        List<Role> roles = server.getRoles();
+
+        Set<User> set = (Set<User>) api.getServerById(serverId).get().getMembers();
+        User[] userArray = set.toArray(new User[set.size()]);
+        List<User> allUserList = set.stream().collect(Collectors.toList());
+        List<User> allOnlineList = set.stream().filter(x -> x.getStatus().equals(UserStatus.ONLINE)).collect(Collectors.toList());
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+
+            }
+
+            if (event.getMessageAuthor().getId() == ownerId) {
+
+                if (event.getMessageContent().equalsIgnoreCase("!ggAllUser")) {
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, memberCount);
+                    //event.getChannel().sendMessage("候選名單有 : ");
+                    //allUserList.stream().forEach(x->event.getChannel().sendMessage((x.getDisplayName(server))));
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage(allUserList.get(randomNum).getDisplayName(server));
+
+                }
+
+                if (event.getMessageContent().equalsIgnoreCase("!ggAllOnline")) {
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, memberCount);
+                    //event.getChannel().sendMessage("候選名單有 : ");
+                    //allOnlineList.stream().forEach(x->event.getChannel().sendMessage((x.getDisplayName(server))));
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage(allOnlineList.get(randomNum).getDisplayName(server));
+
+                }
+
+                if (event.getMessageContent().contains("!ggRoleOnline_")) {
+                    String[] str = event.getMessageContent().split("_");
+                    HashSet<User> rl = (HashSet) roles.stream().filter(x -> x.getName().equals(str[1])).findFirst().get().getUsers();
+                    List<User> result = rl.stream().filter(x -> x.getStatus().equals(UserStatus.ONLINE)).collect(Collectors.toList());
+                    //event.getChannel().sendMessage("候選名單有 : ");
+                    //result.stream().forEach(x->event.getChannel().sendMessage((x.getDisplayName(server))));
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, result.size());
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage(result.get(randomNum).getDisplayName(server));
+
+                }
+
+                if (event.getMessageContent().contains("!ggRoleOffline_")) {
+                    String[] str = event.getMessageContent().split("_");
+                    HashSet<User> rl = (HashSet) roles.stream().filter(x -> x.getName().equals(str[1])).findFirst().get().getUsers();
+                    List<User> result = rl.stream().collect(Collectors.toList());
+                    //event.getChannel().sendMessage("候選名單有 : ");
+                    //result.stream().forEach(x->event.getChannel().sendMessage((x.getDisplayName(server))));
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, result.size());
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage("the winner is :");
+                    event.getChannel().sendMessage(result.get(randomNum).getDisplayName(server));
+
+                }
+
+                if (event.getMessageContent().contains("!roleList")) {
+                    roles.stream().forEach(x -> event.getChannel().sendMessage((x.getName())));
+
+                }
+
+            }
+        });
+
+
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+
     }
 }
